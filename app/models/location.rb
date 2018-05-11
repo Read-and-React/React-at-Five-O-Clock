34d85e38e04626feeff4489c166b5f302ba0ad91
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-10 19:58:14 -0500

    Find Location methods for finding offset and dtz - in Model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,20 @@
 class Location < ApplicationRecord
+
+  def time_diff
+    now = Time.now.strftime("%H:%M").to_i
+    diff_from_now = 17 - now
+    # p diff_from_now
+  end
+
+  def offset
+    local = Time.now.strftime("%z").to_i / 100
+    dtz = local + time_diff
+  end
+
+  def find_location
+    dtz = offset
+    Location.where("tz_offset = #{dtz}")
+
+  end
+
 end
