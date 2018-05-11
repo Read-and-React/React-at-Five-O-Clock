86b257f47fcbc62107ddeb267396f0d926bf14ca
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-10 19:02:58 -0500

    Update seed.rb file, tz_offset as int not string

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,742 +11,679 @@ Location.create([
    city: "Pago Pago",
    country: "American Samoa",
    timezone: "",
-   tz_offset: "-11:00",
+   tz_offset: -11,
    lang_abbr: "sm"
  },
  {
    city: "Papeete",
    country: "French Polynesia",
    timezone: "",
-   tz_offset: "-10:00",
+   tz_offset: -10,
    lang_abbr: "fr"
  },
  {
    city: "Honolulu",
    country: "United States",
    timezone: "",
-   tz_offset: "-10:00",
+   tz_offset: -10,
    lang_abbr: "haw"
  },
  {
    city: "Anchorage",
    country: "United States",
    timezone: "",
-   tz_offset: "-9:00",
+   tz_offset: -9,
    lang_abbr: "en"
  },
  {
    city: "Los Angeles",
    country: "United States",
    timezone: "",
-   tz_offset: "-8:00",
+   tz_offset: -8,
    lang_abbr: "en"
  },
  {
    city: "Vancouver",
    country: "Canada",
    timezone: "",
-   tz_offset: "-8:00",
+   tz_offset: -8,
    lang_abbr: "en"
  },
  {
    city: "Tijuana",
    country: "Mexico",
    timezone: "",
-   tz_offset: "-8:00",
+   tz_offset: -8,
    lang_abbr: "es"
  },
  {
    city: "Phoenix",
    country: "United States",
    timezone: "",
-   tz_offset: "-7:00",
+   tz_offset: -7,
    lang_abbr: "en"
  },
  {
    city: "Ciudad Juarez",
    country: "Mexico",
    timezone: "",
-   tz_offset: "-7:00",
+   tz_offset: -7,
    lang_abbr: "es"
  },
  {
    city: "Calgary",
    country: "Canada",
    timezone: "",
-   tz_offset: "-7:00",
+   tz_offset: -7,
    lang_abbr: "en"
  },
  {
    city: "Guatemala City",
    country: "Guatemala",
    timezone: "",
-   tz_offset: "-6:00",
+   tz_offset: -6,
    lang_abbr: "es"
  },
  {
    city: "Chicago",
    country: "United States",
    timezone: "",
-   tz_offset: "-6:00",
+   tz_offset: -6,
    lang_abbr: "en"
  },
  {
    city: "Mexico City",
    country: "Mexico",
    timezone: "",
-   tz_offset: "-6:00",
+   tz_offset: -6,
    lang_abbr: "es"
  },
  {
    city: "Winnipeg",
    country: "Canada",
    timezone: "",
-   tz_offset: "-6:00",
+   tz_offset: -6,
    lang_abbr: "en"
  },
  {
    city: "Bogota",
    country: "Columbia",
    timezone: "",
-   tz_offset: "-5:00",
+   tz_offset: -5,
    lang_abbr: "es"
  },
  {
    city: "New York",
    country: "United States",
    timezone: "",
-   tz_offset: "-5:00",
+   tz_offset: -5,
    lang_abbr: "en"
  },
  {
    city: "Toronto",
    country: "Canada",
    timezone: "",
-   tz_offset: "-5:00",
+   tz_offset: -5,
    lang_abbr: "en"
  },
  {
    city: "Lima",
    country: "Peru",
    timezone: "",
-   tz_offset: "-5:00",
+   tz_offset: -5,
    lang_abbr: "es"
  },
  {
    city: "Kingston",
    country: "Jamaica",
    timezone: "",
-   tz_offset: "-5:00",
+   tz_offset: -5,
    lang_abbr: "en"
  },
  {
    city: "Halifax",
    country: "Canada",
    timezone: "",
-   tz_offset: "-4:00",
+   tz_offset: -4,
    lang_abbr: "en"
  },
  {
    city: "Caracas",
    country: "Venezuela",
    timezone: "",
-   tz_offset: "-4:00",
+   tz_offset: -4,
    lang_abbr: "es"
  },
  {
    city: "La Paz",
    country: "Bolivia",
    timezone: "",
-   tz_offset: "-4:00",
+   tz_offset: -4,
    lang_abbr: "es"
  },
  {
    city: "Santiago",
    country: "Chile",
    timezone: "",
-   tz_offset: "-4:00",
+   tz_offset: -4,
    lang_abbr: "es"
- },
- {
-   city: "St. John's",
-   country: "Canada",
-   timezone: "",
-   tz_offset: "-3:30",
-   lang_abbr: "en"
  },
  {
    city: "Sao Paulo",
    country: "Brazil",
    timezone: "",
-   tz_offset: "-3:00",
+   tz_offset: -3,
    lang_abbr: "es"
  },
  {
    city: "Buenos Aires",
    country: "Argentina",
    timezone: "",
-   tz_offset: "-3:00",
+   tz_offset: -3,
    lang_abbr: "es"
  },
  {
    city: "Montevideo",
    country: "Uruguay",
    timezone: "",
-   tz_offset: "-3:00",
+   tz_offset: -3,
    lang_abbr: "es"
  },
  {
    city: "Grytviken",
    country: "South Georgia",
    timezone: "",
-   tz_offset: "-2:00",
+   tz_offset: -2,
    lang_abbr: "en"
  },
  {
    city: "Ponta Delgada",
    country: "Portugal",
    timezone: "",
-   tz_offset: "-1:00",
+   tz_offset: -1,
    lang_abbr: "pt"
  },
  {
    city: "Praia",
    country: "Cape Verde",
    timezone: "",
-   tz_offset: "-1:00",
+   tz_offset: -1,
    lang_abbr: "pt"
  },
  {
    city: "Casablanca",
    country: "Morocco",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "ar"
  },
  {
    city: "Dublin",
    country: "Ireland",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "ga"
  },
  {
    city: "Edinburgh",
    country: "Scotland",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "en"
  },
  {
    city: "Lisbon",
    country: "Portugal",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "pt"
  },
  {
    city: "London",
    country: "England",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "en"
  },
  {
    city: "Dakar",
    country: "Senegal",
    timezone: "",
-   tz_offset: "0:00",
+   tz_offset: 0,
    lang_abbr: "fr"
  },
  {
    city: "Amsterdam",
    country: "Netherlands",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "nl"
  },
  {
    city: "Belgrade",
    country: "Serbia",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "sr"
  },
  {
    city: "Berlin",
    country: "Germany",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "de"
  },
  {
    city: "Bratislava",
    country: "Slovakia",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "sk"
  },
  {
    city: "Budapest",
    country: "Hungary",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "hu"
  },
  {
    city: "Copenhagen",
    country: "Denmark",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "da"
  },
  {
    city: "Ljubljana",
    country: "Slovenia",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "sl"
  },
  {
    city: "Madrid",
    country: "Spain",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "es"
  },
  {
    city: "Paris",
    country: "France",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "fr"
  },
  {
    city: "Prague",
    country: "Czech Republic",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "cs"
  },
  {
    city: "Rome",
    country: "Italy",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "it"
  },
  {
    city: "Sarajevo",
    country: "Bosnia and Herzegovina",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "bs"
  },
  {
    city: "Skopje",
    country: "Macedonian",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "sq"
  },
  {
    city: "Stockholm",
    country: "Sweden",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "sv"
  },
  {
    city: "Vienna",
    country: "Austria",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "de"
  },
  {
    city: "Warsaw",
    country: "Poland",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "pl"
  },
  {
    city: "Zagreb",
    country: "Croatia",
    timezone: "",
-   tz_offset: "+1:00",
+   tz_offset: 1,
    lang_abbr: "hr"
  },
  {
    city: "Athens",
    country: "Greece",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "el"
  },
  {
    city: "Bucharest",
    country: "Romania",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "ro"
  },
  {
    city: "Cairo",
    country: "Egypt",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "ar"
  },
  {
    city: "Helsinki",
    country: "Finland",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "fi"
  },
  {
    city: "Jerusalem",
    country: "Isreal",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "iw"
  },
  {
    city: "Kaliningrad",
    country: "Russia",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "ru"
  },
  {
    city: "Kiev",
    country: "Ukrain",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "uk"
  },
  {
    city: "Riga",
    country: "Latvia",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "lv"
  },
  {
    city: "Sofia",
    country: "Bulgaria",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "bg"
  },
  {
    city: "Tallinn",
    country: "Estonia",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "et"
  },
  {
    city: "Vilnius",
    country: "Lithuania",
    timezone: "",
-   tz_offset: "+2:00",
+   tz_offset: 2,
    lang_abbr: "lt"
  },
  {
    city: "Baghdad",
    country: "Iraq",
    timezone: "",
-   tz_offset: "+3:00",
+   tz_offset: 3,
    lang_abbr: "ku"
  },
  {
    city: "Istanbul",
    country: "Turkey",
    timezone: "",
-   tz_offset: "+3:00",
+   tz_offset: 3,
    lang_abbr: "tr"
  },
  {
    city: "Minsk",
    country: "Belarus",
    timezone: "",
-   tz_offset: "+3:00",
+   tz_offset: 3,
    lang_abbr: "be"
  },
  {
    city: "Moscow",
    country: "Russia",
    timezone: "",
-   tz_offset: "+3:00",
+   tz_offset: 3,
    lang_abbr: "ru"
  },
  {
    city: "Nairobi",
    country: "Kenya",
    timezone: "",
-   tz_offset: "+3:00",
+   tz_offset: 3,
    lang_abbr: "en"
- },
- {
-   city: "Tehran",
-   country: "Iran",
-   timezone: "",
-   tz_offset: "+3:30",
-   lang_abbr: "fa"
  },
  {
    city: "Abu Dhabi",
    country: "Uinted Arab Emirates",
    timezone: "",
-   tz_offset: "+4:00",
+   tz_offset: 4,
    lang_abbr: "ar"
  },
  {
    city: "Baku",
    country: "Azerbaijan",
    timezone: "",
-   tz_offset: "+4:00",
+   tz_offset: 4,
    lang_abbr: "az"
  },
  {
    city: "Muscat",
    country: "Muscat",
    timezone: "",
-   tz_offset: "+4:00",
+   tz_offset: 4,
    lang_abbr: "ar"
  },
  {
    city: "Tbilisi",
    country: "Georgia",
    timezone: "",
-   tz_offset: "+4:00",
+   tz_offset: 4,
    lang_abbr: "ka"
  },
  {
    city: "Yerevan",
    country: "Armenia",
    timezone: "",
-   tz_offset: "+4:00",
+   tz_offset: 4,
    lang_abbr: "hy"
- },
- {
-   city: "Kabul",
-   country: "Afghanistan",
-   timezone: "",
-   tz_offset: "+4:30",
-   lang_abbr: "ps"
  },
  {
    city: "Karachi",
    country: "Pakistan",
    timezone: "",
-   tz_offset: "+5:00",
+   tz_offset: 5,
    lang_abbr: "ur"
  },
  {
    city: "Tashkent",
    country: "Uzbekistan",
    timezone: "",
-   tz_offset: "+5:00",
+   tz_offset: 5,
    lang_abbr: "uz"
- },
- {
-   city: "Mumbai",
-   country: "India",
-   timezone: "",
-   tz_offset: "+5:30",
-   lang_abbr: "hi"
- },
- {
-   city: "Colombo",
-   country: "Sri Lanka",
-   timezone: "",
-   tz_offset: "+5:30",
-   lang_abbr: "si"
- },
- {
-   city: "Kathmandu",
-   country: "Nepal",
-   timezone: "",
-   tz_offset: "+5:45",
-   lang_abbr: "ne"
  },
  {
    city: "Astana",
    country: "Kazakhstan",
    timezone: "",
-   tz_offset: "+6:00",
+   tz_offset: 6,
    lang_abbr: "kk"
  },
  {
    city: "Dhaka",
    country: "Bangladesh",
    timezone: "",
-   tz_offset: "+6:00",
+   tz_offset: 6,
    lang_abbr: "bn"
- },
- {
-   city: "Yangon",
-   country: "Myanmar",
-   timezone: "",
-   tz_offset: "+6:30",
-   lang_abbr: "my"
  },
  {
    city: "Bangkok",
    country: "Thailand",
    timezone: "",
-   tz_offset: "+7:00",
+   tz_offset: 7,
    lang_abbr: "th"
  },
  {
    city: "Hanoi",
    country: "Vietnam",
    timezone: "",
-   tz_offset: "+7:00",
+   tz_offset: 7,
    lang_abbr: "vi"
  },
  {
    city: "Jakarta",
    country: "Indonesia",
    timezone: "",
-   tz_offset: "+7:00",
+   tz_offset: 7,
    lang_abbr: "id"
  },
  {
    city: "Beijing",
    country: "China",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "zh-CN"
  },
  {
    city: "Kuala Lumpur",
    country: "Malaysia",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "ms"
  },
  {
    city: "Perth",
    country: "Australia",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "en"
  },
  {
    city: "Singapore",
    country: "Singapore",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "en"
  },
  {
    city: "Taipei",
    country: "Taiwan",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "zh-CN"
  },
  {
    city: "Ulaanbaatar",
    country: "Mongolia",
    timezone: "",
-   tz_offset: "+8:00",
+   tz_offset: 8,
    lang_abbr: "mn"
  },
  {
    city: "Seoul",
    country: "South Korea",
    timezone: "",
-   tz_offset: "+9:00",
+   tz_offset: 9,
    lang_abbr: "ko"
  },
  {
    city: "Tokyo",
    country: "Japan",
    timezone: "",
-   tz_offset: "+9:00",
+   tz_offset: 9,
    lang_abbr: "ja"
- },
- {
-   city: "Adelaide",
-   country: "Australia",
-   timezone: "",
-   tz_offset: "+9:30",
-   lang_abbr: "en"
  },
  {
    city: "Guam",
    country: "Guam",
    timezone: "",
-   tz_offset: "+10:00",
+   tz_offset: 10,
    lang_abbr: "en"
  },
  {
    city: "Port Moresby",
    country: "Papua New Guinea",
    timezone: "",
-   tz_offset: "+10:00",
+   tz_offset: 10,
    lang_abbr: "en"
  },
  {
    city: "Sydney",
    country: "Australia",
    timezone: "",
-   tz_offset: "+10:00",
+   tz_offset: 10,
    lang_abbr: "en"
  },
  {
    city: "Noumea",
    country: "New Caledonia",
    timezone: "",
-   tz_offset: "+11:00",
+   tz_offset: 11,
    lang_abbr: "fr"
  },
  {
    city: "Auckland",
    country: "New Zealand",
    timezone: "",
-   tz_offset: "+12:00",
+   tz_offset: 12,
    lang_abbr: "en"
  },
  {
    city: "Suva",
    country: "Fiji",
    timezone: "",
-   tz_offset: "+12:00",
-   lang_abbr: "en"
- },
- {
-   city: "Waitangi",
-   country: "New Zealand",
-   timezone: "",
-   tz_offset: "+12:45",
+   tz_offset: 12,
    lang_abbr: "en"
  },
  {
    city: "Nuku'alofa",
    country: "Tonga",
    timezone: "",
-   tz_offset: "+13:00",
+   tz_offset: 13,
    lang_abbr: "en"
  },
  {
    city: "Apia",
    country: "Somoa",
    timezone: "",
-   tz_offset: "+13:00",
+   tz_offset: 13,
    lang_abbr: "en"
  },
  {
    city: "Tarawa",
    country: "Kiribati",
    timezone: "",
-   tz_offset: "+14:00",
+   tz_offset: 14,
    lang_abbr: "en"
  }
 ])
