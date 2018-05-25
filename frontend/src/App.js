c54f6bf1a5223966729e2704aa1593178ec05daf
Tyler Damme <tyler.damme@gmail.com>
2018-05-24 20:17:28 -0500

    map function for cities in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,23 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    locations: []
+    locations: [],
+    cities: ""
   };
   componentDidMount() {
     axios.get(`http://localhost:3001/location`).then(res => {
       const locations = res.data;
+      const cities = locations.map(c => c.city);
       this.setState({ locations });
+      this.setState({ cities });
       console.log(this.state.locations);
+      console.log(this.state.cities);
     });
   }
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>{this.state.cities}</h1>
       </div>
     );
   }
