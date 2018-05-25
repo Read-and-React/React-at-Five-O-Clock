312cd66d1e711e21a5c4b140dc295cced64550c2
Cole Challand <ctchalland@gmail.com>
2018-05-24 19:01:06 -0500

    Add location controller

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,18 +4,18 @@ class Location < ApplicationRecord
     where(tz_offset: (Time.now.strftime("%z").to_i / 100) + (16 - Time.now.strftime("%H:%M").to_i)) 
   end
 
-  # def self.time_diff
-  #   now = Time.now.strftime("%H:%M").to_i
-  #   if Time.now.dst?
-  #     diff_from_now = 16 - now
-  #   else
-  #     diff_from_now = 17 - now
-  #   end
-  # end
+  def self.time_diff
+    now = Time.now.strftime("%H:%M").to_i
+    if Time.now.dst?
+      diff_from_now = 16 - now
+    else
+      diff_from_now = 17 - now
+    end
+  end
 
-  # def self.offset
-  #   local = Time.now.strftime("%z").to_i / 100
-  #   dtz = local + time_diff
-  # end
+  def self.offset
+    local = Time.now.strftime("%z").to_i / 100
+    dtz = local + time_diff
+  end
 
 end
