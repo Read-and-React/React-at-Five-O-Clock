ac80a69ff1960d6e64aa70e2c5e1917599d9e8f5
Tyler Damme <tyler.damme@gmail.com>
2018-06-14 20:47:59 -0500

    WIP - Google translate setup, needs debugging

--- frontend/src/components/Phrase.js
+++ frontend/src/components/Phrase.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
-const googleTranslate = require("google-translate");
+import { googleTranslate } from "google-translate";
 
 class Phrase extends Component {
-  // Stop collapsing
   componentDidUpdate() {
-    console.log(this.props.language);
+    this.translatePhrase();
   }
+  translatePhrase() {
+    googleTranslate.translate("My name is Brandon", "es", translation => {
+      console.log(translation.translatedText);
+      return translation.translatedText;
+      // =>  Mi nombre es Brandon
+    });
+  }
+
   render() {
-    return <p />;
+    return <p>hello</p>;
   }
 }
 export default Phrase;
